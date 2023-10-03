1f7f271d90eb680a2195b3170a3d09b94b724eb5
venkadesh004 <venkadeshdeveloper@gmail.com>
2023-10-03 19:19:46 +0530

    Completed Company and Suppliers

--- controllers/supplierControllers.js
+++ controllers/supplierControllers.js
@@ -107,9 +107,33 @@ const acceptContract = async (req, res) => {
             },
           }
         )
-          .then((output) => {
+          .then(async (output) => {
             // console.log(output);
-            res.status(204).json(output);
+            // res.status(204).json(output);
+            await CompanySchema.findOne({email: data["compMail"]}).then(async newOutput => {
+              var compStatusArray = newOutput.status;
+              var newData = {
+                "supID": data["_id"],
+                "status": "ongoing"
+              };
+
+              compStatusArray.push(newData);
+
+              await CompanySchema.updateOne({
+                email: data["compMail"]
+              }, {
+                $set: {
+                  status: compStatusArray
+                }
+              }).then(finalOutput => {
+                console.log(finalOutput);
+                res.status(204).json(finalOutput);
+              }).catch(err => {
+                res.status(500).json({err: "Internal server Error!"});
+              })
+            }).catch(err => {
+              res.status(500).json({err: "Internal server Error!"});
+            })
           })
           .catch((err) => {
             // console.log(err);
@@ -218,24 +242,91 @@ const downloadCompanyFile = async (req, res) => {
 };
 
 const getSupplierID = async (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader("Content-Type", "text/plain");
   try {
     const data = req.params.id;
-    await SupplierSchema.find({"_id": data}).then(result => {
-      if (result.length === 0) {
-        res.status(404).json({err: "User not found!"});
-      } else {
-        res.status(200).json(result);
-      }
-    }).catch(err => {
-      console.log(err);
-      res.status(500).json({err: "Internal server Error!"});
-    });
+    await SupplierSchema.find({ _id: data })
+      .then((result) => {
+        if (result.length === 0) {
+          res.status(404).json({ err: "User not found!" });
+        } else {
+          res.status(200).json(result);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ err: "Internal server Error!" });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Internal server Error!" });
+  }
+};
+
+const getRequests = async (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+
+  try {
+    const data = req.params.id;
+    await SupplierSchema.find({ _id: data })
+      .then(async (result) => {
+        var emailArray = [];
+        // console.log(result[0]["status"]);
+        await result[0]["status"].forEach(element => {
+          if (element["state"] === "requested") {
+            emailArray.push(element["compMail"]);
+          }
+        });
+        console.log(emailArray);
+        CompanySchema.find({email: {$in: emailArray}}).then(output => {
+          console.log(output);
+          res.status(200).json(output);
+        }).catch(err => {
+          console.log(err);
+          res.status(500).json({err: "Internal server Error!"});
+        })
+      })
+      .catch((err) => {
+        res.status(500).json({ err: "Internal server Error!" });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Internal server Error!" });
+  }
+};
+
+const getAcceptedStatus = async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  try {
+    const data = req.params.id;
+
+    await SupplierSchema.find({ _id: data })
+      .then(async (result) => {
+        var emailArray = [];
+        // console.log(result[0]["status"]);
+        await result[0]["status"].forEach(element => {
+          if (element["state"] === "ongoing") {
+            emailArray.push(element["compMail"]);
+          }
+        });
+        console.log(emailArray);
+        CompanySchema.find({email: {$in: emailArray}}).then(output => {
+          console.log(output);
+          res.status(200).json(output);
+        }).catch(err => {
+          console.log(err);
+          res.status(500).json({err: "Internal server Error!"});
+        })
+      })
+      .catch((err) => {
+        res.status(500).json({ err: "Internal server Error!" });
+      });
   } catch (err) {
     console.log(err);
     res.status(500).json({err: "Internal server Error!"});
   }
-};
+}
 
 module.exports.getSupplier = getSupplier;
 module.exports.addSupplier = addSupplier;
@@ -245,3 +336,5 @@ module.exports.rejectContract = rejectContract;
 module.exports.downloadFiles = downloadFiles;
 module.exports.downloadCompanyFile = downloadCompanyFile;
 module.exports.getSupplierID = getSupplierID;
+module.exports.getRequests = getRequests;
+module.exports.getAcceptedStatus = getAcceptedStatus;
--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Router = express.Router();
 
-const { getSupplier, addSupplier, editProfile, acceptContract, rejectContract, downloadFiles, downloadCompanyFile, getSupplierID } = require('../controllers/supplierControllers');
+const { getSupplier, addSupplier, editProfile, acceptContract, rejectContract, downloadFiles, downloadCompanyFile, getSupplierID, getRequests, getAcceptedStatus } = require('../controllers/supplierControllers');
 
 Router.route('/getSupplier').post(getSupplier);
 Router.route('/addSupplier').post(addSupplier);
@@ -11,5 +11,7 @@ Router.route('/rejectContract').put(rejectContract);
 Router.route('/downloadFiles/:id').get(downloadFiles);
 Router.route('/downloadCompanyFile').get(downloadCompanyFile);
 Router.route('/getSupplierID/:id').get(getSupplierID);
+Router.route('/getRequests/:id').get(getRequests);
+Router.route('/getAcceptedStatus/:id').get(getAcceptedStatus);
 
 module.exports = Router;
