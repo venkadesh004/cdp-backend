d3396d3dfdcdda51632d6b7918e296ab869ab086
venkadesh004 <venkadeshdeveloper@gmail.com>
2023-10-04 01:59:16 +0530

    Added company verification

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Router = express.Router();
 
-const { getAdmin, addAdmin, getUnapprovedData, getApprovedData, allowSuppliers, denySuppliers, downloadFiles, getAdminID } = require('../controllers/adminControllers');
+const { getAdmin, addAdmin, getUnapprovedData, getApprovedData, allowSuppliers, denySuppliers, downloadFiles, getAdminID, allowCompanies, denyCompanies, downloadCompanyFIles, getUnapprovedCompData, getApprovedCompData } = require('../controllers/adminControllers');
 
 Router.route('/getAdmin').post(getAdmin);
 Router.route('/addAdmin').post(addAdmin);
@@ -9,7 +9,12 @@ Router.route('/getUnapprovedData').get(getUnapprovedData);
 Router.route('/getApprovedData').get(getApprovedData);
 Router.route('/allowSuppliers').put(allowSuppliers);
 Router.route('/denySuppliers').put(denySuppliers);
-Router.route('/downloadFiles').get(downloadFiles);
+Router.route('/downloadFiles/:id').get(downloadFiles);
 Router.route('/getAdminID/:id').get(getAdminID);
+Router.route('/allowCompanies').put(allowCompanies);
+Router.route('/denyCompanies').put(denyCompanies);
+Router.route('/downloadCompanyFIles/:id').get(downloadCompanyFIles);
+Router.route('/getApprovedCompData').get(getApprovedCompData);
+Router.route('/getUnapprovedCompData').get(getUnapprovedCompData);
 
 module.exports = Router;
